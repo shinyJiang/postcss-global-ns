9399d0f98e5ea6f48cfc002725cd37128f290567
Shiny <jshanshan1021@126.com>
2019-03-04 22:49:29 +0800

    postcss

---
+++ index.js
@@ -0,0 +1,28 @@
+const postcss = require('postcss');
+
+module.exports = postcss.plugin('postcss-global-ns', opts => {
+	
+	opts = opts || {};
+  let globalClsNames = opts.hasOwnProperty('globalClsNames') && opts.globalClsNames
+  return root => {
+    // console.log(root.source.input.file)
+		root.walk(node => {
+			let selectorArr = (node.selector || '').split(',')
+			return node.selector = ((selectorArr || []).map((itm) => {
+				if (itm.match(/^(\s*)(html|body)(\s*)$/)) {
+					return itm
+        }
+        let clsName = globalClsNames.find(v=>{
+          return root.source && root.source.input.file.indexOf(v.filePath) >=0
+        })
+        if(clsName){
+          return clsName.selector + ' ' + itm
+        } else {
+          return itm
+        }
+			}).join(','))
+		})
+
+	};
+
+})
