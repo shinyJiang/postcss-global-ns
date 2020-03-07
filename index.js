3de276b336f54ae650adcff928eb3bb492dac6f2
Shiny <jshanshan1021@126.com>
2020-03-07 15:03:48 +0800

    格式化

--- index.js
+++ index.js
@@ -4,22 +4,21 @@ module.exports = postcss.plugin('postcss-global-ns', opts => {
   opts = opts || {};
   let globalClsNames = opts.hasOwnProperty('globalClsNames') && opts.globalClsNames
   return root => {
-		root.walk(node => {
-			let selectorArr = (node.selector || '').split(',')
-			return node.selector = ((selectorArr || []).map((itm) => {
-				if (itm.match(/^(\s*)(html|body)(\s*)$/)) {
-					return itm
+    root.walk(node => {
+      let selectorArr = (node.selector || '').split(',')
+      return node.selector = ((selectorArr || []).map((itm) => {
+        if (itm.match(/^(\s*)(html|body)(\s*)$/)) {
+          return itm
         }
-        let clsName = globalClsNames.find(v=>{
-          return root.source && root.source.input.file.indexOf(v.filePath) >=0
+        let clsName = globalClsNames.find(v => {
+          return root.source && root.source.input.file.indexOf(v.filePath) >= 0
         })
-        if(clsName){
+        if (clsName) {
           return clsName.selector + ' ' + itm
         } else {
           return itm
         }
-			}).join(','))
-		})
-	};
-
+      }).join(','))
+    })
+  };
 })
